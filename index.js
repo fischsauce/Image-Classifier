da3a7605e2fdcc0be2b6eda879c373bc0d724a3a
Steve Fisher <fishsauce@protonmail.com>
2019-08-26 02:31:51 +0800

    Embedded a K-Nearest Neighbour classifier to allow custom training

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let net;
 
 const webcamElement = document.getElementById('webcam');
+const classifier = knnClassifier.create();
+
 
 //Boilerplate for loading the webcam:
 async function setupWebcam() {
@@ -36,17 +38,48 @@ async function app() {
 
   //wait for the webcam to initialize:
   await setupWebcam();
-  
+
+  // while (true) {
+  //   const result = await net.classify(webcamElement);
+  //
+  //   document.getElementById('console').innerText = `
+  //     prediction: ${result[0].className}\n
+  //     probability: ${result[0].probability}
+  //   `;
+  //
+  //   // Give some breathing room by waiting for the next animation frame to
+  //   // fire.
+  //   await tf.nextFrame();
+  // }
+
+  // Reads an image from the webcam and associates it with a specific class index.
+  const addExample = classId => {
+    // Get the intermediate activation of MobileNet
+    // 'conv_preds' and pass that to the KNN classifier.
+    const activation = net.infer(webcamElement, 'conv_preds');
+
+    // Pass the intermediate activation to the classifier.
+    classifier.addExample(activation, classId);
+  };
+
+  // When clicking a button, add an example for that class.
+  document.getElementById('class-a').addEventListener('click', () => addExample(0));
+  document.getElementById('class-b').addEventListener('click', () => addExample(1));
+  document.getElementById('class-c').addEventListener('click', () => addExample(2));
+
   while (true) {
-    const result = await net.classify(webcamElement);
+    if (classifier.getNumClasses() > 0) {
+      // Get the activation from mobilenet from the webcam.
+      const activation = net.infer(webcamElement, 'conv_preds');
+      // Get the most likely class and confidences from the classifier module.
+      const result = await classifier.predictClass(activation);
 
-    document.getElementById('console').innerText = `
-      prediction: ${result[0].className}\n
-      probability: ${result[0].probability}
-    `;
-
-    // Give some breathing room by waiting for the next animation frame to
-    // fire.
+      const classes = ['A', 'B', 'C'];
+      document.getElementById('console').innerText = `
+        prediction: ${classes[result.classIndex]}\n
+        probability: ${result.confidences[result.classIndex]}
+      `;
+    }
     await tf.nextFrame();
   }
 }
