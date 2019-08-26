dd9de72e0b0347267369fa59f8ba5ae4c143e375
Steve Fisher <fishsauce@protonmail.com>
2019-08-26 23:05:14 +0800

    Added some styling. Refined the no class solution

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+//// STILL TO DO:
+
+// add no class
+// allow image upload
+// capture first 30 frames
+
 let net;
 
 const webcamElement = document.getElementById('webcam');
@@ -31,26 +37,8 @@ async function app() {
   net = await mobilenet.load();
   console.log('Sucessfully loaded model');
 
-  // Make a prediction through the model on our image.
-  // const imgEl = document.getElementById('img');
-  // const result = await net.classify(imgEl);
-  // console.log(result);
-
   //wait for the webcam to initialize:
   await setupWebcam();
-
-  // while (true) {
-  //   const result = await net.classify(webcamElement);
-  //
-  //   document.getElementById('console').innerText = `
-  //     prediction: ${result[0].className}\n
-  //     probability: ${result[0].probability}
-  //   `;
-  //
-  //   // Give some breathing room by waiting for the next animation frame to
-  //   // fire.
-  //   await tf.nextFrame();
-  // }
 
   // Reads an image from the webcam and associates it with a specific class index.
   const addExample = classId => {
@@ -67,28 +55,43 @@ async function app() {
   document.getElementById('class-b').addEventListener('click', () => addExample(1));
   document.getElementById('class-c').addEventListener('click', () => addExample(2));
 
+  // document.getElementById('ul-class-a').addEventListener('click', () => addExample(0));
+  // document.getElementById('ul-class-b').addEventListener('click', () => addExample(1));
+  // document.getElementById('ul-class-c').addEventListener('click', () => addExample(2));
+
+
   while (true) {
     if (classifier.getNumClasses() > 0) {
       // Get the activation from mobilenet from the webcam.
       const activation = net.infer(webcamElement, 'conv_preds');
       // Get the most likely class and confidences from the classifier module.
       const result = await classifier.predictClass(activation);
-
-
-
       const classes = ['A', 'B', 'C'];
 
-      //console.log(result.confidences);
+      // Create a dictionary for adding highlight to the buttons:
+      const class_dict = {A: '#class-a', B: '#class-b', C: '#class-c'};
 
-      if((result.confidences[result.classIndex]) <= 0.667) {
+      //Create a threshold so we avoid prediction if the confidence is too low:
+      if((result.confidences[result.classIndex.value]) <= 0.667) {
         document.getElementById('console').innerText = `
-          *** ALERT! *** I have no idea what that is! \n
-          probability: ${result.confidences[result.classIndex].toFixed(2)} is too to risk my robotic reputation! \n Trust your own judgement, Human!
+          Prediction: Hmmm... 'No Class' \n
+          Probability: ${(result.confidences[result.classIndex]* 100).toFixed(2) + '%'}
+          <strong>is too low!</strong> Trust your own judgement, Human!
         `;
-      } else {document.getElementById('console').innerText = `
-        prediction: ${classes[result.classIndex]}\n
-        probability: ${result.confidences[result.classIndex].toFixed(2)}
-      `;
+      } else {
+
+        // Remove the existing highlights class from buttons:
+        $('*.button_capture').removeClass('highlight');
+
+        // Label our positive result & add the highlight:
+        var elem = classes[result.classIndex];
+        $(class_dict[elem]).addClass('highlight');
+
+        // Do a little formatting to get % instead of a float, then output the prediction:
+        document.getElementById('console').innerText = `
+        Prediction: ${classes[result.classIndex]}\n
+        Probability: ${(result.confidences[result.classIndex] * 100).toFixed(2) + '%'}
+        `;
       }
     }
     await tf.nextFrame();
