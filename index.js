746c9cc520178c234e7c3105d81af253e49955b4
Steve Fisher <fishsauce@protonmail.com>
2019-08-27 04:15:19 +0800

    Added no-class function. More stylings. Status messages.

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-//// STILL TO DO:
-
-// add no class
-// allow image upload
-// capture first 30 frames
-
 let net;
 
 const webcamElement = document.getElementById('webcam');
 const classifier = knnClassifier.create();
-
 
 //Boilerplate for loading the webcam:
 async function setupWebcam() {
@@ -30,68 +23,97 @@ async function setupWebcam() {
   });
 }
 
+// Main:
 async function app() {
-  console.log('Loading mobilenet..');
+  //console.log('Loading mobilenet..');
 
   // Load the model.
   net = await mobilenet.load();
-  console.log('Sucessfully loaded model');
+  //console.log('Sucessfully loaded model');
 
   //wait for the webcam to initialize:
   await setupWebcam();
 
   // Reads an image from the webcam and associates it with a specific class index.
   const addExample = classId => {
-    // Get the intermediate activation of MobileNet
-    // 'conv_preds' and pass that to the KNN classifier.
+    // Get the intermediate activation of MobileNet 'conv_preds' and pass that to the KNN classifier.
     const activation = net.infer(webcamElement, 'conv_preds');
-
     // Pass the intermediate activation to the classifier.
     classifier.addExample(activation, classId);
   };
 
-  // When clicking a button, add an example for that class.
-  document.getElementById('class-a').addEventListener('click', () => addExample(0));
-  document.getElementById('class-b').addEventListener('click', () => addExample(1));
-  document.getElementById('class-c').addEventListener('click', () => addExample(2));
+// Create a timer to capture a few frames for the initial 'No-Class' state:
 
-  // document.getElementById('ul-class-a').addEventListener('click', () => addExample(0));
-  // document.getElementById('ul-class-b').addEventListener('click', () => addExample(1));
-  // document.getElementById('ul-class-c').addEventListener('click', () => addExample(2));
+// Inform the user not to move the webcam:
+document.getElementById('console').innerText = `
+      CALIBRATING BASE CONDITIONS:
+      Please wait a few seconds...
+`;
 
+// Capture the no class frames from webcam:
+  var timesRun = 0;
+  var interval = setInterval(function initNoClass(){
+      timesRun += 1;
+      if(timesRun === 10){
+          clearInterval(interval);
+      }
+      addExample(0);
+      document.getElementById("loading_bar").append("*");
+  }, 500);
 
+// Display loading complete message:
+  setTimeout(loadComplete, 5000);
+  function loadComplete(){
+      document.getElementById("loading_bar").innerText = "Ready for Calissification!";
+      document.getElementById("console").innerText = "<< This is the Empty Class >>";
+  }
+
+// Route input from the buttons
+  document.getElementById('class-a').addEventListener('click', () => addExample(1));
+  document.getElementById('class-b').addEventListener('click', () => addExample(2));
+  document.getElementById('class-c').addEventListener('click', () => addExample(3));
+
+// Run our samples from the webcam against the model:
   while (true) {
-    if (classifier.getNumClasses() > 0) {
+    if (classifier.getNumClasses() > 1) {
       // Get the activation from mobilenet from the webcam.
       const activation = net.infer(webcamElement, 'conv_preds');
-      // Get the most likely class and confidences from the classifier module.
-      const result = await classifier.predictClass(activation);
-      const classes = ['A', 'B', 'C'];
+      var result = await classifier.predictClass(activation);
+      const classes = ['E', 'A', 'B', 'C'];
 
       // Create a dictionary for adding highlight to the buttons:
-      const class_dict = {A: '#class-a', B: '#class-b', C: '#class-c'};
+      const class_dict = {E: '', A: '#class-a', B: '#class-b', C: '#class-c'};
 
-      //Create a threshold so we avoid prediction if the confidence is too low:
-      if((result.confidences[result.classIndex.value]) <= 0.667) {
+      // Check for 'No-Class':
+      if ((classes[result.classIndex]) == 'E') {
         document.getElementById('console').innerText = `
-          Prediction: Hmmm... 'No Class' \n
-          Probability: ${(result.confidences[result.classIndex]* 100).toFixed(2) + '%'}
-          <strong>is too low!</strong> Trust your own judgement, Human!
+          Prediction: This is the class of 'No Class'
         `;
-      } else {
-
         // Remove the existing highlights class from buttons:
         $('*.button_capture').removeClass('highlight');
-
-        // Label our positive result & add the highlight:
-        var elem = classes[result.classIndex];
-        $(class_dict[elem]).addClass('highlight');
-
-        // Do a little formatting to get % instead of a float, then output the prediction:
-        document.getElementById('console').innerText = `
-        Prediction: ${classes[result.classIndex]}\n
-        Probability: ${(result.confidences[result.classIndex] * 100).toFixed(2) + '%'}
-        `;
+      }
+      else {
+        //Create a threshold, so we avoid prediction if the confidence is too low:
+        if ((result.confidences[result.classIndex]) <= 0.67) {
+          document.getElementById('console').innerText = `
+            Prediction: Hmmm ... uncertainty \n
+            Probability: ${(result.confidences[result.classIndex] * 100).toFixed(2) + '%'} is too low!  Trust your own judgement, Human!
+          `;
+          // Remove the existing highlights class from buttons:
+          $('*.button_capture').removeClass('highlight');
+        }
+        else {
+          // Do a little formatting to get a % instead of float, then output the prediction:
+          document.getElementById('console').innerText = `
+            Prediction: ${classes[result.classIndex]}\n
+            Probability: ${(result.confidences[result.classIndex] * 100).toFixed(2) + '%'}
+          `;
+          // Remove the existing highlights class from buttons:
+          $('*.button_capture').removeClass('highlight');
+          // Label our positive result & add the highlight:
+          var elem = classes[result.classIndex];
+          $(class_dict[elem]).addClass('highlight');
+        }
       }
     }
     await tf.nextFrame();
